8ffd8c86db86222628c64e2ed148214fbe17016f
Coby Tamayo <ctamayo@sitecrafting.com>
2020-10-29 17:49:16 -0700

    bump version

--- greg.php
+++ greg.php
@@ -6,7 +6,7 @@
  * Author: SiteCrafting
  * Author URI: https://www.sitecrafting.com/
  * Description: A de-coupled calendar solution for WordPress and Timber
- * Version: 0.0.1
+ * Version: 0.1.0
  * Requires PHP: 7.4
  */
 
