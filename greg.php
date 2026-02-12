bb1d5fd15b7347ff750dd97e989f35493bce0bbf
Phil M Price <philmprice@Mac.click-network.com>
2026-02-11 16:19:50 -0800

    Updated version number

--- greg.php
+++ greg.php
@@ -6,7 +6,7 @@
  * Author: SiteCrafting
  * Author URI: https://www.sitecrafting.com/
  * Description: A de-coupled calendar solution for WordPress and Timber
- * Version: 2.0.2
+ * Version: 2.0.7
  * Requires PHP: 7.4 or 8
  */
 
